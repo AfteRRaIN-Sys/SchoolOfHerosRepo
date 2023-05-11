16fd12d0414a267614c5eb7b4cf96d0353668375
Salakapoi <reviewgamerkla@gmail.com>
2023-05-11 15:21:12 +0700

    [Change] Main Menu Revamp
    Using the designed layout and rewrite the button script

--- Assets/Scripts/MainMenu Script/MainMenuBtn.cs
+++ Assets/Scripts/MainMenu Script/MainMenuBtn.cs
@@ -4,23 +4,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class StartGameBtn : MonoBehaviour
+public class MainMenuBtn : MonoBehaviour
 {
     // Start is called before the first frame update
     public Button button;
 
     public GameStateSO gameStateSO;
 
+    /*
     public void Start(){
+        
         button = this.transform.GetComponentInParent<Button>();
         button.onClick.AddListener(delegate () { this.ButtonClicked(); });
-    }
-    
-    public void InitGame() {
-        gameStateSO.money = 1000;
-        gameStateSO.point = 700;
-        gameStateSO.studentList = new List<Student>();
-        gameStateSO.professorList = new List<Professor>();
+
     }
 
     public void ButtonClicked()
@@ -29,8 +25,35 @@ public class StartGameBtn : MonoBehaviour
         InitGame();
         NextScene();
     }
-       void NextScene()
+    */
+    public void StartGame()
+    {
+        Debug.Log("Starting Game");
+        InitGame();
+        NextScene();
+    }
+
+    public void Tutorial()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quiting Game");
+        Application.Quit();
+    }
+    void InitGame()
+    {
+        gameStateSO.money = 1000;
+        gameStateSO.point = 700;
+        gameStateSO.studentList = new List<Student>();
+        gameStateSO.professorList = new List<Professor>();
+    }
+
+    void NextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
 }
